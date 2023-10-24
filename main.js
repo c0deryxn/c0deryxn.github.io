8cfd24f0985a909eebfbebcafe4ee496385f681f
박성민(Seongmin Park) <gabriel3894@kakao.com>
2023-10-25 01:14:24 +0900

    main.js

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-let fruit = prompt("무슨 과일을 사고 싶나요?");
-
-switch (fruit) {
-    case "사과":
-        alert('100원입니다.');
-        break;
-    case "바나나":
-        alert("200원입니다.");
-        break;
-    case "키위":
-        alert("300원입니다.");
-        break;
-    case "멜론":
-        alert("500원입니다.");
-        break;
-    case "수박":
-        alert("500원입니다.");
-        break;
-    default:
-        alert("그런 과일 없어요...");
+function sayHello(name) {
+    alert(`Hello, ${name}`);
 }
+
+sayHello("박성민");
+sayHello("좌수정");
