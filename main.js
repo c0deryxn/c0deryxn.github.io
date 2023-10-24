5944ee520452382632ab8fa1c3467abb2d5ab0f1
박성민(Seongmin Park) <gabriel3894@kakao.com>
2023-10-24 16:22:36 +0900

    test -> index

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-while (true) {
-    for (var i = 0; i < 10; i++) {
-        prompt(i);
+const PASSWORD = "test1234$";
+
+isVerified = () => {
+    var userInput = propmt("비밀번호");
+    if (userInput != PASSWORD) {
+        alert("비밀번호 불일치");
+        isVerified();
+    } else {
+        alert('인증되었습니다.');
     }
 }
+
+isVerified();
