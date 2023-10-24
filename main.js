b4deb4c566961fba3c9d8b938486b873df93877a
박성민(Seongmin Park) <gabriel3894@kakao.com>
2023-10-25 00:37:08 +0900

    switch구문 버그 수정 완료

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ let fruit = prompt("무슨 과일을 사고 싶나요?");
 switch (fruit) {
     case "사과":
         alert('100원입니다.');
+        break;
     case "바나나":
         alert("200원입니다.");
+        break;
     case "키위":
         alert("300원입니다.");
+        break;
     case "멜론":
         alert("500원입니다.");
+        break;
     case "수박":
         alert("500원입니다.");
+        break;
 }
