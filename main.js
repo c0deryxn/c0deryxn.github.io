1525ed19f10e2ca767abcaa4a5477ebe8f39837c
박성민(Seongmin Park) <gabriel3894@kakao.com>
2023-10-24 16:44:09 +0900

    index.html, main.js 연동

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// 상수 정의 part
 const PASSWORD = "test1234$";
 
+// 함수 정의 part
 isVerified = () => {
     var userInput = prompt("비밀번호");
     if (userInput != PASSWORD) {
@@ -8,6 +10,10 @@ isVerified = () => {
     } else {
         alert('인증되었습니다.');
     }
+}
+
+alertInputValue = (userId, userPw) => {
+    alert(`아이디: ${userId} 비밀번호: ${userPw} 감지되었습니다.`)
 }
 
 isVerified();
